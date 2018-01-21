58121f605e1067ff3426d07eec544c7f61e0c48f
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 23:42:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,11 @@ def prime?(a)
   elsif a <= 1 || a % 2 == 0
     false
   else
-    (3..a-1) 
+    (3..a-1).to_a.each do |i|
+      if a % i == 0
+        false
+      end
+    end
+    true
   end
 end
